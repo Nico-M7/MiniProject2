cdc480bc8e362c8c9a3ce10b560a363c8c9e2ff2
BuildTools <unconfigured@null.spigotmc.org>
2021-11-13 15:05:02 -0500

    find all categories

--- FileReader.js
+++ FileReader.js
@@ -3,26 +3,21 @@ const jobs = require('./jobs.json');  //stores json file in a variable
 //loop through the json file, if a job category doesn't exist add that category to the array, once its looped through once, 
 //loop again check how many times each category occurs, print the name of the category that occurs the most and the amount of times
 
-function Category(name){
-    let occurance = 1;
-    this.name = name;
-    function countOccurance(){
-        occurance++;
-    }
-}
-
-let jobCategories = [];
+let categories = [];
 
 for(let job in jobs) {
     for(let category of jobs[job].categories) {
-        if(jobCategories.includes(category.name)){
-            jobCategories.category;
-        }
-        else{
-            jobCategories.push(category);
+        if(!categories.includes(category)){
+            categories.push(category);
         }
     } 
 }
 
-console.log(jobCategories)
+console.log(categories)
+
+let jobsWithCategory = [];
+
+
+
+
 
